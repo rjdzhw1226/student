ba63c8cd62983458bcfe35a39045a88f1784dd2f
rjdzhw1226 <cf656779436@163.com>
2023-06-23 21:28:11 +0800

    成绩后端 家

---
+++ src/main/java/com/student/controller/ScoreController.java
@@ -0,0 +1,60 @@
+package com.student.controller;
+
+import com.student.pojo.page;
+import com.student.pojo.pageBean;
+import com.student.pojo.student;
+import com.student.service.ScoreService;
+import com.student.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+
+@RequestMapping("/score")
+@RestController
+public class ScoreController {
+
+    @Value("${student.path}")
+    private String basePath;
+
+    @Autowired
+    private StudentService studentService;
+
+    @Autowired
+    private ScoreService scoreService;
+
+    @RequestMapping("/download/{name}")
+    public void download(@PathVariable("name") String name, HttpServletResponse response){
+
+        try {
+            FileInputStream fileInputStream = new FileInputStream(new File(basePath + name));
+            ServletOutputStream outputStream = response.getOutputStream();
+            response.setContentType("image/jpeg");
+            int len = 0;
+            byte[] bytes = new byte[1024];
+            while ((len = fileInputStream.read(bytes)) != -1){
+                outputStream.write(bytes,0,len);
+                outputStream.flush();
+            }
+            outputStream.close();
+            fileInputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @RequestMapping("/All")
+    @ResponseBody
+    public page<student> selectAll(@RequestBody pageBean page){
+        Integer Page = page.getPage();
+        Integer Size = page.getSize();
+        page<student> students = studentService.queryAll(Page, Size);
+        return students;
+    }
+}
+
---
+++ src/main/java/com/student/service/ScoreService.java
@@ -0,0 +1,19 @@
+package com.student.service;
+
+import com.student.pojo.score;
+import com.student.pojo.student;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ScoreService {
+
+    public List<score> queryScore(){
+        return null;
+    }
+
+    public List<student> queryAll(){
+        return null;
+    }
+}
