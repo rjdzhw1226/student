9c312bd143063f1b9e98a9a8e06a2e3e8f52460c
任嘉冬 <11606722+ren-jiadong@user.noreply.gitee.com>
2023-06-27 17:41:57 +0800

    暂时 班

--- src/main/java/com/student/StudentDemoApplication.java
+++ src/main/java/com/student/StudentDemoApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.student.**"})
 @EnableAsync
+@EnableTransactionManagement
 public class StudentDemoApplication {
 
     public static void main(String[] args) {
