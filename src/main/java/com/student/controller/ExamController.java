d27b1d7d7bada36413d622344da13968821c5666
rjdzhw1226 <cf656779436@163.com>
2023-07-04 22:25:50 +0800

    前端 家

---
+++ src/main/java/com/student/controller/ExamController.java
@@ -0,0 +1,10 @@
+package com.student.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/exam")
+public class ExamController {
+
+}
