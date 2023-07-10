79f4b481afa75393e6339dfc12cc147b3559eb7a
任嘉冬 <11606722+ren-jiadong@user.noreply.gitee.com>
2023-07-10 17:33:55 +0800

    提交 班

---
+++ src/main/java/com/student/filter/JwtAutherticationTokenFilter.java
@@ -0,0 +1,18 @@
+package com.student.filter;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtAutherticationTokenFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
