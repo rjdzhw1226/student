a5c2ca99ac5e5a653f49862c2cf120243cbdfbf5
563424 <656779436@qq.com>
2023-07-14 09:26:59 +0800

    测试账号

--- src/main/java/com/student/Constant/RedisKey.java
+++ src/main/java/com/student/Constant/RedisKey.java
@@ -1,6 +1,7 @@
 package com.student.Constant;
 
 public class RedisKey {
+
     public static final String CACHE_SUB_KEY = "cache:sub:";
     public static final String CACHE_SUB_COUNT_KEY = "cache:sub:count:";
     public static final String CACHE_SUB_CHOOSE_KEY = "cache:sub:choose:";
